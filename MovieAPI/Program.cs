145e1dcdc714e2a1be5c4025dddacbc966635d1d
Embe0519 <emmamberg1997@gmail.com>
2023-08-22 17:09:58 +0200

    Commit(Added config for swagger documentation)

--- MovieAPI/Program.cs
+++ MovieAPI/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MovieAPI.DataAccess;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,18 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+
+        Title = "Film API", Version = "v1",
+        Description = "Film-API concists of an Entity Framework Code First workflow and an ASP.NET Core Web API in C#. /n The project folder includes a database diagram made in lucid-charts. The project is a datastore of movies, characters and franchises.",
+        Contact = new OpenApiContact { Name = "Emma Berg & Martin Lucena" };
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+});
+
 
 var app = builder.Build();
 
